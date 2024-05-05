66839ab7587fc8a8d9b46a5c2859109d316abd24
Geoffrey Coste <geoffreycoste13@gmail.com>
2024-05-05 17:08:15 +0200

    Cleaning starter template

---
+++ app/layout.jsx
@@ -0,0 +1,24 @@
+import { DM_Sans, Bricolage_Grotesque } from "next/font/google";
+import "./globals.css";
+
+export const dm_sans = DM_Sans({
+  variable: "--font-dm-sans",
+  subsets: ["latin"],
+});
+export const bricolage_grotesque = Bricolage_Grotesque({
+  variable: "--font-bricolage-grotesque",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "claesens",
+  description: "Emilie Claesens, Designer basée à Paris.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={dm_sans.variable}>{children}</body>
+    </html>
+  );
+}
